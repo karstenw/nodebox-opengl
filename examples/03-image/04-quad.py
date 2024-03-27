07522baae1161ec3aa682f7da14128acb67ab736
Karsten Wolf <karstenwo@web.de>
2024-03-27 16:08:00 +0100

    Update 04-quad.py

--- examples/03-image/04-quad.py
+++ examples/03-image/04-quad.py
@@ -10,11 +10,16 @@ img.quad.dy1 =  100
 img.quad.dx2 =  100
 img.quad.dy2 = -100
 
+X,Y = 500,500
+x2,y2 = round(X/2), round(Y/2)
+
 # This flushes the image cache, so it is a costly operation.
 
 def draw(canvas):
+    img.quad.dx2 = -x2 + canvas.mouse.x 
+    img.quad.dy2 = -y2 + canvas.mouse.y 
     canvas.clear()
     image(img)
     
-canvas.size = 500, 500
+canvas.size = X,Y
 canvas.run(draw)
